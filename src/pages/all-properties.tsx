e59c013ee2df8b6e7b198ec2196f65555f3da6dd
mr-hollowman <selvamani825@gmail.com>
2023-02-17 17:15:08 +0530

    filter logics

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,38 +1,139 @@
 import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
-import { Box, Typography, Stack } from "@pankod/refine-mui";
+import { Box, Typography, Stack, TextField, Select, MenuItem } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 import { CustomButton, PropertyCard } from "components";
+import { useMemo } from "react";
 const AllProperties = () => {
   const navigate = useNavigate();
 
   const {
-    tableQueryResult: { data, isLoading, isError }
+    tableQueryResult: { data, isLoading, isError },
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter, setSorter,
+    filters, setFilters
   } = useTable();
   const allProperties = data?.data ?? [];
-  if(isLoading) return <Typography>Loading...</Typography>
-  if(isError) return <Typography>Error...</Typography>
+
+  const currentPrice = sorter.find((item) => item.field === 'price')?.order
+
+  const toggleSort = (field: string) => {
+    setSorter([{ field, order: currentPrice === 'asc' ? 'desc' : 'asc' }])
+  }
+
+  const currentFilterValues = useMemo(() => {
+    const logicalFilters = filters.flatMap((item) => ("field" in item ? item : []));
+    return {
+      title: logicalFilters.find((item) => item.field === 'title')?.value || "",
+      propertyType: logicalFilters.find(item => item.field === 'propertyType')?.value || "",
+    }
+  }, [filters])
+
+  if (isLoading) return <Typography>Loading...</Typography>
+  if (isError) return <Typography>Error...</Typography>
   return (
     <Box>
+
+      <Box mt={"20px"} sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        <Stack direction={"column"} width="100%">
+          <Typography fontSize={25} fontWeight={700} color="#11142D">{!allProperties.length ? "There are no properties" : "All properties"}</Typography>
+          <Box mb={2} display="flex" width="84%" justifyContent={"space-between"} flexWrap="wrap">
+            <Box display="flex" gap={2} flexWrap="wrap" mb={{ xs: "20px", sm: 0 }}>
+              <CustomButton
+                title={`Sort price ${currentPrice === 'asc' ? "↑" : "↓"}`}
+                handleClick={() => toggleSort('price')}
+                backgroundColor="#475BE8"
+                color="#FCFCFC"
+              />
+              <TextField
+                variant='outlined'
+                color="info"
+                placeholder="Search by title"
+                onChange={(e) => {
+                  setFilters([{ field: 'title', operator: 'contains', value: e.currentTarget.value ? e.currentTarget.value : undefined }])
+                }}
+                value={currentFilterValues.title}
+              />
+
+              <Select
+                variant="outlined"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ "aria-label": "Without label" }}
+                defaultValue=""
+                value={currentFilterValues.propertyType}
+                onChange={(e) => {
+                  setFilters([{ field: 'propertyType', operator: 'eq', value: e.target.value }], 'replace')
+                }}
+              >
+                <MenuItem value="">All</MenuItem>
+                {["Apartment", "Villa", "Farmhouse", "Condos", "Townhouse", "Duplex", "Studio", "Chalet"].map((item) => (
+                  <MenuItem key={item} value={item.toLowerCase()}>{item}</MenuItem>
+                ))}
+              </Select>
+            </Box>
+          </Box>
+        </Stack>
+      </Box>
+
       <Stack direction={"row"} justifyContent="space-between" alignItems={"center"}>
-        <Typography fontSize={25} fontWeight={700} color="#11142D">All Properties</Typography>
         <CustomButton title="Add Property" handleClick={() => navigate("/properties/create")} backgroundColor="#475BE8" color="#FCFCFC" icon={<Add />} />
       </Stack>
 
       <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
-        {allProperties.map((property)=>{
-          return(
+        {allProperties.map((property) => {
+          return (
             <PropertyCard
-            key={property._id}
-            id={property._id}
-            title={property.title}
-            price={property.price}
-            location={property.location}
-            photo = {property.photo}
-             />
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              price={property.price}
+              location={property.location}
+              photo={property.photo}
+            />
           )
         })}
       </Box>
+
+      {allProperties.length > 0 && (
+        <Box mt={3} display={"flex"} gap={2} flexWrap="wrap" >
+          <CustomButton
+            title="Previous"
+            handleClick={() => setCurrent(prev => prev - 1)}
+            backgroundColor="#475BE8"
+            color="$FCFCFC"
+            disabled={!(current > 1)}
+          />
+          <Box display={{ xs: "hidden", sm: "flex" }} alignItems="center" gap={"5px"}>
+            Page{""}<strong>{current} of {pageCount}</strong>
+          </Box>
+
+          <CustomButton
+            title="Next"
+            handleClick={() => setCurrent(prev => prev + 1)}
+            backgroundColor="#475BE8"
+            color="$FCFCFC"
+            disabled={current === pageCount}
+          />
+          <Select
+            variant="outlined"
+            color="info"
+            displayEmpty
+            required
+            inputProps={{ "aria-label": "Without label" }}
+            defaultValue={10}
+            onChange={() => { }}
+          >
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>Show {size}</MenuItem>
+            ))}
+          </Select>
+        </Box>
+      )}
     </Box>
   )
 }
